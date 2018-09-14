4feb1766767c9de54747becf2b75cf91d6071a37
okbrown <orlando.brown@jobsite.co.uk>
2018-09-14 20:58:29 +0100

    Add handling of videos/movies

--- middleware/plex-sessions/x-user-handler.js
+++ middleware/plex-sessions/x-user-handler.js
@@ -4,45 +4,46 @@ const { getSessions } = require('./session-handler');
 const { violationMessage } = require('./session-violation-handler');
 
 const isUserSessionsX = () => {
-	return async (req, res, next) => {
-		const { Account: { title }, Player: { uuid } } = res.locals.payload;
-		try {
-			const sessions = await getSessions();
-			const users = _getUserSession(sessions, title);
-			const violation = _hasUserViolatedMultipleSessions(users);
+  return async (req, res, next) => {
+    const { Account: { title }, Player: { uuid } } = res.locals.payload;
+    try {
+      const sessions = await getSessions();
+      const users = _getUserSession(sessions, title);
+      const violation = _hasUserViolatedMultipleSessions(users);
 
-			if (violation) {
-				const error = violationMessage({ status: true, reason: 'duplicateUser', user: title, uuid });
-				next(error);
-			}
-			else {
-				next();
-			}
-		}
-		catch (error) {
-			res.status(400);
-		}
-	};
+      if (violation) {
+        const error = violationMessage({ status: true, reason: 'duplicateUser', user: title, uuid });
+        next(error);
+      }
+      else {
+        next();
+      }
+    }
+    catch (error) {
+      res.status(400);
+    }
+  };
 };
 
 const _getUserSession = (sessions, user) => {
-	const { MediaContainer: { size, Metadata } } = sessions;
-	if (size > 1) {
-		return Metadata
-			.map(item => _getUser(item, user))
-			.filter(Boolean);
-	}
-	return false;
+  const { MediaContainer: { size, Metadata, Video } } = sessions;
+  const data = Metadata || Video;
+  if (size > 1) {
+    return data
+      .map(item => _getUser(item, user))
+      .filter(Boolean);
+  }
+  return false;
 };
 
 const _getUser = (item, user) => {
-	if (item.User.title === user) {
-		return item.User.title;
-	}
+  if (item.User.title === user) {
+    return item.User.title;
+  }
 };
 
 const _hasUserViolatedMultipleSessions = users => users && users.length > 1;
 
 module.exports = {
-	isUserSessionsX
+  isUserSessionsX
 };
